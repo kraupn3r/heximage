8aa09dbb82932bbf89b52ce8327a3d2bd123b198
kraupn3r <m.pyzowski@gmail.com>
2022-07-05 14:09:16 +0200

    rm staticfile dir

--- heximage/settings/dev.py
+++ heximage/settings/dev.py
@@ -135,11 +135,8 @@ MIDDLEWARE_CLASSES = (
     'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 # Rrestrict file size in bytes
