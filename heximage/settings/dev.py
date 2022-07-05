22ac5c17c712b687236c4f0095582c6fc030673a
kraupn3r <m.pyzowski@gmail.com>
2022-07-05 15:57:23 +0200

    rm staticfiles dirs

--- heximage/settings/dev.py
+++ heximage/settings/dev.py
@@ -130,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
