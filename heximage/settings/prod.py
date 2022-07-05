786315407bfe3d69e1fce95c11c293ae250ab7fd
kraupn3r <m.pyzowski@gmail.com>
2022-07-05 16:52:49 +0200

    rm staticfiles dirs

--- heximage/settings/prod.py
+++ heximage/settings/prod.py
@@ -25,7 +25,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),)
+
 STATIC_ROOT = 'staticfiles'
 AWS_S3_REGION_NAME = env('AWS_REGION')
