2463654e643800dd675757bc2e9bd8c45b50f9f3
kraupn3r <m.pyzowski@gmail.com>
2022-07-05 16:10:14 +0200

    rm custom storages

--- heximage/settings/prod.py
+++ heximage/settings/prod.py
@@ -6,14 +6,6 @@ from .dev import *
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    file_overwrite = False
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
-]
-
 ############
 # DATABASE #
 ############
@@ -37,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_LOCATION = 'static'
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
