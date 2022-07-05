1ba50ec2bdfbf5af26001b3c0d8ff65e6bd9e031
kraupn3r <m.pyzowski@gmail.com>
2022-07-05 15:48:30 +0200

    update settings for s3 bucket

--- heximage/settings/prod.py
+++ heximage/settings/prod.py
@@ -43,10 +43,11 @@ ALLOWED_HOSTS = ['*']
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_LOCATION = 'static'
-AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
 # Depending on the AWS account used, you might also need to declare AWS_SESSION_TOKEN as an environment variable
 DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
